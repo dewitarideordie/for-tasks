323629accf5fec880dc66e5008190530fbdc090c
rideordie <diajengdewita@gmail.com>
2026-01-21 07:37:23 +0700

    Refactor guessing loop with error handling
    Added error handling for user input and congratulatory message.

--- MAIN.py
+++ MAIN.py
@@ -4,9 +4,12 @@ secret_number = 280808
 
 guests = int(input("Guess the number: "))
 
-while secret_number != guests:
-    print("Poor you, you're stuck in this loop forever!")
-    print("Try guess again")
-    guests = int(input("Guess the number: "))
+try:
+    while secret_number != guests:
+        print("Poor you, you're stuck in this loop forever!")
+        print("Try guess again")
+        guests = int(input("Guess the number: "))
+    print("Congartulations,you did it!")
 
-print("Congartulations,you did it!")
+except:
+    print("Oh, no! You put the wrong input :P")
